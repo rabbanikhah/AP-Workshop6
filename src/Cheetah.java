9f47d6eb6c80fdb72700b8d59d68d9bf1835a835
rabbanikhah <rabbanikhah.neda@gmail.com>
2024-12-01 17:01:34 +0330

    third edit for defind interface

--- src/Cheetah.java
+++ src/Cheetah.java
@@ -12,4 +12,9 @@ public class Cheetah extends Mammal implements Hunter{
     public void show (){
         System.out.println("name: " + name + ", age: " + age + ", speed : " + speed + ", " + info);
     }
+
+    @Override
+    public void hunt(Prey prey) {
+        System.out.println(this.name + " hunted " + prey.getName());
+    }
 }
